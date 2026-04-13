b84dc8bf63f53c08cedd53d7bcc5b940d7e15378
Thomas Jung <thomas.jung@sap.com>
2026-04-13 19:13:34 -0400

    fix: remove blank browser import from main.go (will be imported in cmd/resources.go)

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	_ "github.com/pkg/browser"
-
-	"github.tools.sap/developer-relations/sap-devs-cli/cmd"
-)
+import "github.tools.sap/developer-relations/sap-devs-cli/cmd"
 
 func main() {
 	cmd.Execute()
 }
+
